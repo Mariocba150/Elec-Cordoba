a0ac52cf3510e4f751d50c55af16318b1128ff5d
Mario Aguilar <mariocba150@gmail.com>
2022-07-24 18:59:47 -0300

    Agrego variables al archivo presupuestos.php

---
+++ php/presupuestos.php
@@ -0,0 +1,18 @@
+<?php
+
+$nombrePresup = $_POST ['presupuestoNombre'];
+$apellidoPresup = $_POST ['presupuestoApellido'];
+$telefonoPresup = $_POST ['presupuestoTelefono'];
+$emailPresup = $_POST ['presupuestoCorreo'];
+$tipoInstalacion = $_POST ['instalacion'];
+$tipoReparacion = $_POST ['reparacion'];
+$tipoCertificacion = $_POST ['certificacion'];
+$circuitoMono = $_POST ['monofasica'];
+$circuitoTri = $_POST ['trifasica'];
+$conMateriales = $_POST ['conMateriales'];
+$sinMateriales = $_POST ['sinMateriales'];
+$trabajoDetalles = $_POST ['presupuestoDetalles'];
+
+
+
+?>
